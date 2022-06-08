a0ec92eedb9abc07ca001e30175e88ee74f0374e
wagner <wagner.santos@accurate.com.br>
2022-06-07 23:01:05 -0300

    Added functions

--- variables.go
+++ variables.go
@@ -1,40 +1,18 @@
 package main
 
-import "fmt"
-import "reflect"
-
+import ( 
+	"fmt"
+	"reflect"
+	"os"
+)
 
 
 func main(){
-	label := "Start" // same thing that var label string = "Start"
-	var name string
-	var age int
-	var height float32
-
-	fmt.Println("######", label, "######")
-	fmt.Println("Hi! Please, what's your name:")
-	fmt.Scan(&name)
-
-	fmt.Println("Hi", name)
-	fmt.Println("Please, how old are you?")
-
-	fmt.Scan(&age)
-	fmt.Println("Oh really?")
-
-	fmt.Println("And what's your height, please?")
-	fmt.Scan(&height)
+	greetings(true)
 	
-	fmt.Println("\nType Name: ", reflect.TypeOf(name))
-	fmt.Println("Type Age: ", reflect.TypeOf(age))
-	fmt.Println("Type Height: ", reflect.TypeOf(height))
-
-	fmt.Println("\n#### MENU ####")
-	fmt.Println("1) Start monitoring.")
-	fmt.Println("2) Show logs.")
-	fmt.Println("3) Exit")
+	menu()
 	
-	var command int
-	fmt.Scan(&command)
+	command := readCommand()
 
 	// if command == 1 {
 	// 	fmt.Println("Starting monitoring...")
@@ -53,7 +31,48 @@ func main(){
 		fmt.Println("Showing logs...")
 	case 3:
 		fmt.Println("Exiting...")
+		os.Exit(0) // expected exit
 	default:
 		fmt.Println("Invalid command!")
+		os.Exit(-1)  // unexpected exit
 	}
+}
+
+func greetings(show_type bool){
+	label := "Start" // same thing that var label string = "Start"
+	var name string
+	var age int
+	var height float32
+
+	fmt.Println("######", label, "######")
+	fmt.Println("Hi! Please, what's your name:")
+	fmt.Scan(&name)
+
+	fmt.Println("Hi", name)
+	fmt.Println("Please, how old are you?")
+
+	fmt.Scan(&age)
+	fmt.Println("Oh really?")
+
+	fmt.Println("And what's your height, please?")
+	fmt.Scan(&height)
+
+	if show_type{
+		fmt.Println("\nType Name: ", reflect.TypeOf(name))
+		fmt.Println("Type Age: ", reflect.TypeOf(age))
+		fmt.Println("Type Height: ", reflect.TypeOf(height))
+	}
+}
+
+func menu(){
+	fmt.Println("\n#### MENU ####")
+	fmt.Println("1) Start monitoring.")
+	fmt.Println("2) Show logs.")
+	fmt.Println("3) Exit")
+}
+
+func readCommand() int{
+	var command int
+	fmt.Scan(&command)
+	return command
 }
