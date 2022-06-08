4f61ab5b6949988345419499708fd2b2c8801dce
wagner <wagner.santos@accurate.com.br>
2022-06-07 22:26:13 -0300

    if/else instructions

--- variables.go
+++ variables.go
@@ -19,12 +19,31 @@ func main(){
 	fmt.Println("Please, how old are you?")
 
 	fmt.Scan(&age)
-	fmt.Println(age, "!! Oh really?")
+	fmt.Println("Oh really?")
 
 	fmt.Println("And what's your height, please?")
 	fmt.Scan(&height)
 	
-	fmt.Println("Type Name: ", reflect.TypeOf(name))
+	fmt.Println("\nType Name: ", reflect.TypeOf(name))
 	fmt.Println("Type Age: ", reflect.TypeOf(age))
 	fmt.Println("Type Height: ", reflect.TypeOf(height))
+
+	fmt.Println("\n#### MENU ####")
+	fmt.Println("1) Start monitoring.")
+	fmt.Println("2) Show logs.")
+	fmt.Println("3) Exit")
+	
+	var command int
+	fmt.Scan(&command)
+
+	if command == 1 {
+		fmt.Println("Starting monitoring...")
+	} else if command == 2 {
+		fmt.Println("Showing logs...")
+	} else if command == 3 {
+		fmt.Println("Exiting...")
+	} else {
+		fmt.Printf("%d: Invalid command!", &command)
+	}
+
 }
