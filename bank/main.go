c47e62ec8aca02a5afe8309d7be3f6242c9b42b9
wagner <wagner.santos@accurate.com.br>
2022-06-15 22:43:59 -0300

    Added Withdraw function and changed to English

--- bank/main.go
+++ bank/main.go
@@ -2,33 +2,47 @@ package main
 
 import "fmt"
 
-type ContaCorrente struct{
-	titular string
-	numeroAgencia int
-	numeroConta int
-	saldo float64
+type CheckingAccount struct{
+	owner string
+	agencyNumber int
+	accountNumber int
+	balance float64
 }
 
+
+func (c *CheckingAccount) Withdraw(value float64)string {
+	allowedWithdrawal := value > 0 && value <= c.balance 
+	if allowedWithdrawal{
+		c.balance -= value
+		return "Withdrawal successful."
+	}else {
+		return "Insufficient balance or invalid value."
+	}
+}
+
+
 func main(){
-	wagner := ContaCorrente{
-		titular: "Wagner Herculano", 
-		numeroAgencia: 012, 
-		numeroConta: 3456,
-		saldo: 10720,
+	wagner := CheckingAccount{
+		owner: "Wagner Herculano", 
+		agencyNumber: 012, 
+		accountNumber: 3456,
+		balance: 10720,
 	}
 
-	matheus := ContaCorrente{"Matheus", 123, 4567, 10531.0}
+	matheus := CheckingAccount{"Matheus", 123, 4567, 10531.0}
 
-	var danielle *ContaCorrente
-	danielle = new(ContaCorrente)
-	danielle.titular = "Danielle"
-	danielle.numeroAgencia = 321
-	danielle.numeroConta = 7890
-	danielle.saldo = 4852
+	var danielle *CheckingAccount
+	danielle = new(CheckingAccount)
+	danielle.owner = "Danielle"
+	danielle.agencyNumber = 321
+	danielle.accountNumber = 7890
+	danielle.balance = 4852
 	// when comparing values using this way above (x == y) we need to use pointers (*x == y*)
 	// otherwise the address will be compared (&x == &y)
 
 	fmt.Println(wagner)
 	fmt.Println(matheus)
 	fmt.Println(*danielle)
+
+	fmt.Println(wagner.Withdraw(-10))
 }
