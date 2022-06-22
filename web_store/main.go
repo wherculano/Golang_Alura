4ce583839c3c5a26903bdc0155eb4620f90b1245
wagner <wagner.santos@accurate.com.br>
2022-06-21 22:17:43 -0300

    Created struct Product

--- web_store/main.go
+++ web_store/main.go
@@ -6,6 +6,14 @@ import (
 )
 
 
+type Product struct{
+	Name string
+	Description string
+	Value float64
+	Amount int
+}
+
+
 var temp8 = template.Must(template.ParseGlob("templates/*.html"))
 
 func main(){
@@ -14,5 +22,11 @@ func main(){
 }
 
 func index(w http.ResponseWriter, r *http.Request){
-	temp8.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{Name: "Earphone", Description: "New!", Value: 12.34, Amount: 6},
+		{"Flash Drive", "32Gb", 9.99, 10},
+		{"Keyboard", "With leds", 20, 4},
+	}
+	
+	temp8.ExecuteTemplate(w, "Index", products)
 }
