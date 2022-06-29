c3389cebc228f4dc0fe54fc2a73c4dbb3e4b5684
wagner <wagner.santos@accurate.com.br>
2022-06-28 21:21:50 -0300

    Created DB connection and displayed all data

--- web_store/main.go
+++ web_store/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq" // _ means excute just during it is running
 )
 
 
+func connectDB() *sql.DB{
+	conn := "user=postgres dbname=alura_store password=123qweasd host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+
 type Product struct{
+	Id int
 	Name string
 	Description string
 	Value float64
@@ -22,11 +36,34 @@ func main(){
 }
 
 func index(w http.ResponseWriter, r *http.Request){
-	products := []Product{
-		{Name: "Earphone", Description: "New!", Value: 12.34, Amount: 6},
-		{"Flash Drive", "32Gb", 9.99, 10},
-		{"Keyboard", "With leds", 20, 4},
+	db := connectDB()
+	selectAllProducts, err := db.Query("select * from products")
+	if err != nil{
+		panic(err.Error())
 	}
 	
-	temp8.ExecuteTemplate(w, "Index", products)
+	product := Product{} // instance of one Product
+	all_products := []Product{}  // slice of Product
+
+	for selectAllProducts.Next(){
+		var id, amount int
+		var name, description string
+		var value float64
+		
+		// scans all lines from DB and save the variables
+		err := selectAllProducts.Scan(&id, &name, &description, &value, &amount)
+		if err != nil{
+			panic(err.Error())
+		}
+
+		product.Name = name
+		product.Description = description
+		product.Value = value
+		product.Amount = amount
+
+		all_products = append(all_products, product)
+	}
+
+	temp8.ExecuteTemplate(w, "Index", all_products)
+	defer db.Close() // Executes all lines and then runs this line
 }
