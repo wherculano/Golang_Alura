9f2ce6c71fb74d446218816c1f6753bd9a701875
wagner <wagner.santos@accurate.com.br>
2022-07-02 00:14:13 -0300

    Added Content-type

--- Go_Rest_API/controllers/controller.go
+++ Go_Rest_API/controllers/controller.go
@@ -15,6 +15,7 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetAllPersonalities(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "application/json")
 	var p []models.Personality
 	database.DB.Find(&p)
 	json.NewEncoder(w).Encode(p)
