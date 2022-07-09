76afcb984940a26a1b67ea9d4ec40e75e859313c
wagner <wagner.santos@accurate.com.br>
2022-07-08 22:42:20 -0300

    Instaled testify, updated test func and  added test content

--- Go_Gin_Rest_API/main_test.go
+++ Go_Gin_Rest_API/main_test.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"Alura/Go_Gin_Rest_API/controllers"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupTestRoutes() *gin.Engine {
@@ -17,10 +19,13 @@ func SetupTestRoutes() *gin.Engine {
 func TestStatusCodeGreeting(t *testing.T) {
 	r := SetupTestRoutes()
 	r.GET("/:name", controllers.Greeting)
-	req, _ := http.NewRequest("GET", "/", nil)
+	req, _ := http.NewRequest("GET", "/Wagner", nil)
 	resp := httptest.NewRecorder()
 	r.ServeHTTP(resp, req)
-	if resp.Code != http.StatusOK {
-		t.Fatalf("Status Code %d is different than %d.", resp.Code, http.StatusOK)
-	}
+	assert.Equal(t, http.StatusOK, resp.Code, "Status Code %d is different than %d.", resp.Code, http.StatusOK)
+
+	mockResponse := `{"message":"Hello Wagner"}`
+	responseBody, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, mockResponse, string(responseBody))
+
 }
